dc2ae537a1433f2a90873ffb49a38f77270da851
Aleksey Bodin <56234934+albodin@users.noreply.github.com>
2025-08-17 21:50:29 -0700

    fix: Update minhook to fix occasional hooking issue where no free memory is found

--- src/version.h
+++ src/version.h
@@ -2,9 +2,9 @@
 
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 17
-#define VERSION_PATCH 6
+#define VERSION_PATCH 7
 
-#define VERSION_STRING "1.17.6"
+#define VERSION_STRING "1.17.7"
 
 // Auto-generated version info
 // This file is automatically updated by version_bump.py
